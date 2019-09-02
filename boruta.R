e5c9583afdfdd801baad320251be590486b0a7fc
sribobv15 <54675397+sribobv15@users.noreply.github.com>
2019-09-02 12:15:39 +0200

    Update boruta.R

--- boruta.R
+++ boruta.R
@@ -1,7 +1,7 @@
 Neopro<- read.csv(file="c:/Users/user/Desktop/weather_reading.csv", header=TRUE, sep=",")
 library(Boruta)
 # Decide if a variable is important or not using Boruta
-borutaoutput <- Boruta(Cancer_reading ~ ., data=na.omit(Neopro), doTrace=2)  # perform Boruta search
+borutaoutput <- Boruta(weather_reading ~ ., data=na.omit(Neopro), doTrace=2)  # perform Boruta search
 # Confirmed 10 attributes: Humidity, Inversion_base_height, Inversion_temperature, Month, Pressure_gradient and 5 more.
 # Rejected 3 attributes: Day_of_month, Day_of_week, Wind_speed.
 borutasignif <- names(borutaoutput$finalDecision[borutaoutput1$finalDecision %in% c("Confirmed", "Tentative")])  # collect Confirmed and Tentative variables
